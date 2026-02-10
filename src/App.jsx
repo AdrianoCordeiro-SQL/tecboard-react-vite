9778492a77f114c4e94b56c328d5dbff853f4d1d
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-02-10 08:37:28 -0300

    Cria e aplica temas personalizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { Board } from "./Pages/Board";
+import { ThemeProvider } from '@mui/material'
+import theme from './theme'
 
 function App() {
  
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Board />
-    </>
+    </ThemeProvider>
   );
 }
 
