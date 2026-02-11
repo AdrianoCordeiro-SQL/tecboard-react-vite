17a9241e98b925679c1574b6f2a2894a5c4436ae
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-02-11 07:37:14 -0300

    Integra o react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { Board } from "./Pages/Board";
 import { ThemeProvider } from '@mui/material'
 import theme from './theme'
+import { Form } from './Pages/Form';
 
 function App() {
  
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Board />
+      <Form />
     </ThemeProvider>
   );
 }
